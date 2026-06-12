41476a3a2248fb836231023f711510530a9513c9
matias <criacomatias@gmail.com>
2026-06-12 14:27:57 -0300

    fix pagina exito compra

---
+++ app/compra/exito/page.tsx
@@ -0,0 +1,10 @@
+export default function ExitoPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1>Compra exitosa</h1>
+        <p>Gracias por tu compra. Te vamos a contactar pronto.</p>
+      </div>
+    </main>
+  );
+}
